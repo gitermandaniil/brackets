b81e6942d31dc82ca9ca7c0af616a389d0fe4908
Daniil Giterman <gitermandaniil@gmail.com>
2020-10-24 15:36:08 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = (str, bracketsConfig) => {
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    let pair = bracketsConfig[i].join('')
+
+    if (str.includes(pair)) {
+      str = str.replace(pair, '')
+      i = -1
+    }
+  }
+
+  return str.length === 0
 }
